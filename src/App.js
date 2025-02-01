4d7e968fdbbe594df7892825ca7c5e2b78d786ef
aman4718 <bholukumar4718@gmail.com>
2025-02-02 02:06:39 +0530

    buildmodel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import './App.css';
-import BuildModel from './components/buildModel';
+import "./App.css";
+import { useState } from "react";
+import XModal from "./XModal.js";
 
 function App() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalOpenBackground, setModalOpenBackground] = useState(false);
+
+  const modalBackground = {
+    height: "100vh",
+    backdropFilter: modalOpenBackground ? "blur(1px)" : "none", // Apply backdrop-filter conditionally
+    transition: "backdrop-filter 0.3s ease", // Add a smooth transition effect
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+
   return (
-    <div className="App">
-     <BuildModel/>
+    <div style={modalBackground}>
+      <h1>User Details Modal</h1>
+      <button
+        className="modalTrigger"
+        onClick={() => {
+          setIsOpenModal(true);
+          setModalOpenBackground(true);
+        }}
+      >
+        Open Form
+      </button>
+      {isOpenModal && (
+        <XModal
+          setIsOpenModal={setIsOpenModal}
+          setModalOpenBackground={setModalOpenBackground}
+        />
+      )}
     </div>
   );
 }
